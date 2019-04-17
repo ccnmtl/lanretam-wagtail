8da8b627ef7a89c5e038063d6a0bfed4cb220be2
Zarina Mustapha <zarina@columbia.edu>
2019-04-17 11:00:25 -0400

    StreamField addition for body content

--- lanretam/main/models.py
+++ lanretam/main/models.py
@@ -1,13 +1,22 @@
 # from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from lanretam.main.blocks import ImageBlock
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    page_body = StreamField([
+        ('text', blocks.RichTextBlock(icon='pilcrow')),
+        ('image', ImageBlock()),
+        ('sr_text', blocks.TextBlock(
+                            required=False,
+                            icon='fa-universal-access',
+                            label='SR text')),
+    ], blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        StreamFieldPanel('page_body'),
     ]
