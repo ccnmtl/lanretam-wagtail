00dfea41f473c4f3af1724686f623f2a0c495264
Zarina Mustapha <zarina@columbia.edu>
2019-04-17 17:09:30 -0400

    Add table block in body streamfield

--- lanretam/main/models.py
+++ lanretam/main/models.py
@@ -3,20 +3,23 @@
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 from wagtail.core import blocks
+from wagtail.contrib.table_block.blocks import TableBlock
 from wagtail.admin.edit_handlers import StreamFieldPanel
 from lanretam.main.blocks import ImageBlock
 
 
 class HomePage(Page):
-    page_body = StreamField([
+    body = StreamField([
         ('text', blocks.RichTextBlock(icon='pilcrow')),
         ('image', ImageBlock()),
+        ('table', TableBlock(template='main/blocks/table_block.html')),
+        ('table_caption', blocks.CharBlock(icon='fa-thumb-tack')),
         ('sr_text', blocks.TextBlock(
                             required=False,
                             icon='fa-universal-access',
                             label='SR text')),
-    ], blank=True)
+    ], verbose_name='Page content', blank=True)
 
     content_panels = Page.content_panels + [
-        StreamFieldPanel('page_body'),
+        StreamFieldPanel('body'),
     ]
