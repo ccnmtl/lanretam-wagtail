ce9f5f067ffee24352661e7ff9aeb07242fba94d
Zarina Mustapha <zarina@columbia.edu>
2019-04-18 16:15:15 -0400

    Reconstructed the blocks that made up a page body

--- lanretam/main/blocks.py
+++ lanretam/main/blocks.py
@@ -1,5 +1,7 @@
 from wagtail.images.blocks import ImageChooserBlock
-from wagtail.core.blocks import CharBlock, StructBlock
+from wagtail.core.blocks import (CharBlock, TextBlock, RichTextBlock,
+                                 StructBlock, StreamBlock)
+from wagtail.contrib.table_block.blocks import TableBlock
 
 
 class ImageBlock(StructBlock):
@@ -14,3 +16,18 @@ class ImageBlock(StructBlock):
     class Meta:
         icon = 'image'
         template = "main/blocks/image_block.html"
+
+
+# StreamBlocks
+class BaseStreamBlock(StreamBlock):
+    """
+    Define the custom blocks that `StreamField` will utilize for content body
+    """
+    text = RichTextBlock(icon='pilcrow')
+    image = ImageBlock()
+    table = TableBlock(template='main/blocks/table_block.html')
+    table_caption = CharBlock(icon='fa-thumb-tack')
+    sr_text = TextBlock(
+                        required=False,
+                        icon='fa-universal-access',
+                        label='SR text')
